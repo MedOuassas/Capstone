963fa35ae0f960afeef9afd3bf139ce470faed59
MedOuassas <hapixsimo@gmail.com>
2024-01-11 11:24:12 +0100

    test home page

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Home page', () => {
+test('renders Home page test', () => {
   render(<App />);
   const linkElement = screen.getByText(/Home page/i);
   expect(linkElement).toBeInTheDocument();
